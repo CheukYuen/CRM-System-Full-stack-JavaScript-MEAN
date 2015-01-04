70f7ada56d72292ed9c1def4a8ab379300146079
Zhuoyuan Lin <winster1851@gmail.com>
2015-01-03 23:15:46 -0800

    edit database url from local to cloud.

--- dbConfig/database.js
+++ dbConfig/database.js
@@ -1,7 +1,7 @@
 module.exports = {
 
 	// the database url to connect
-	//url : 'mongodb://leon:123@ds061620.mongolab.com:61620/contacts'
-	url : 'mongodb://localhost:27017/contact'
+	url : 'mongodb://leon:123@ds061620.mongolab.com:61620/contacts'
+	//url : 'mongodb://localhost:27017/contact'
 
 }
