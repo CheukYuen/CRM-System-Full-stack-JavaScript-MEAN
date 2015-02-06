c48e66046dfabfd5ae3ecb9b54201810841c90bf
Zhuoyuan Lin <winster1851@gmail.com>
2015-02-06 00:35:23 -0800

    cors

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 var express  = require('express');
 var app      = express(); 								// create our app w/ express
 var mongoose = require('mongoose'); 					// mongoose for mongodb
+var cors = require('cors');
 var port  	 = process.env.PORT || 8000; 				// set the port
 var database = require('./dbConfig/database'); 			// load the database config
 
@@ -13,12 +14,12 @@ var methodOverride = require('method-override'); // simulate DELETE and PUT (exp
 mongoose.connect(database.url); 	// connect to mongoDB database on modulus.io
 app.use(express.static(__dirname + '/app')); 				// set the static files location /public/img will be /img for users
 
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-});      // CORS (Cross-Origin Resource Sharing) headers to support Cross-site HTTP requests
-
+//app.use(function(req, res, next) {
+//    res.header("Access-Control-Allow-Origin", "*");
+//    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//    next();
+//});      // CORS (Cross-Origin Resource Sharing) headers to support Cross-site HTTP requests
+app.use(cors());
 app.use('/bower_components', express.static(__dirname + '/bower_components'));
 app.use(morgan('dev')); 										// log every request to the console
 app.use(bodyParser.urlencoded({'extended':'true'})); 			// parse application/x-www-form-urlencoded
